a4f05d79d1c6b42e1a94ed4188e4a8848befdd5b
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-08-20 09:16:38 +0100

    try changing env syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-		api_key: ENV.get('MAILGUN_API_KEY'),
-		domain: ENV.get('MAILGUN_DOMAIN')
+		# api_key: ENV.get('MAILGUN_API_KEY'),
+    # domain: ENV.get('MAILGUN_DOMAIN'),
+    
+    api_key: ENV['MAILGUN_API_KEY'],
+		domain: ENV['MAILGUN_DOMAIN']
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
